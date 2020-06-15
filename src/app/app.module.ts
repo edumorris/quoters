21af4368c5d8fad20c54e9868786834f596707da
edumorris <eduuwachira@gmail.com>
2020-06-15 12:10:46 +0300

    Diplay styling for content added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { DateCountPipe } from './date-count.pipe';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
